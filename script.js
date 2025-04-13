5a78d2700f90642717b42f9a521d1126bd977153
Sormatotra422 <alfalune422@gmail.com>
2025-04-13 21:39:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const boutonCompte = document.getElementById("toggle-compte");
+const sousMenu = document.getElementById("sous-menu-compte");
+
+boutonCompte.addEventListener("click", function(event) {
+  event.preventDefault(); // Empêche le lien de recharger la page
+  sousMenu.classList.toggle("show"); // Affiche ou cache le sous-menu
+});
