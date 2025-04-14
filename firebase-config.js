0c21c858f4fe39aa733e854f8df48252531b4f23
Sormatotra422 <alfalune422@gmail.com>
2025-04-14 11:38:58 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+<!-- SDK Firebase -->
+<script src="https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.11.0/firebase-auth-compat.js"></script>
+<script>
+  const firebaseConfig = {
+    apiKey: "TA_CLE_API",
+    authDomain: "TON_PROJET.firebaseapp.com",
+    projectId: "TON_PROJET",
+    appId: "TON_APP_ID"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
