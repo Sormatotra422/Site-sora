4581ea3a794603afcacee3f0478f7d350455a19a
Sormatotra422 <alfalune422@gmail.com>
2025-04-14 12:13:37 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,23 @@
-<!-- SDK Firebase -->
-<script src="https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.11.0/firebase-auth-compat.js"></script>
-<script>
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "TA_CLE_API",
-    authDomain: "TON_PROJET.firebaseapp.com",
-    projectId: "TON_PROJET",
-    appId: "TON_APP_ID"
+    apiKey: "AIzaSyA9xjNfw9KE_KXqqh3hPQ9yDGrNLC6vaxA",
+    authDomain: "sora-74a5e.firebaseapp.com",
+    projectId: "sora-74a5e",
+    storageBucket: "sora-74a5e.firebasestorage.app",
+    messagingSenderId: "856492665588",
+    appId: "1:856492665588:web:ce390388972ebc06814ca1",
+    measurementId: "G-BGLE81ZKYD"
   };
 
-  firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 </script>
